2f63a9cac17b98d4ff3d2dd6b6260fdb0aa8671f
Christopher Morris <christopher.morris@udo.edu>
2023-03-25 16:45:59 +0100

    - Started implemeting better evaluation protocol, using 10-CV and hyperparmeter optimization on the validation set - Added simple 1-WL-equivalent GraphConv layer

--- GNN_untrained/main_gnn.py
+++ GNN_untrained/main_gnn.py
@@ -3,7 +3,7 @@ from auxiliarymethods.gnn_evaluation import gnn_evaluation
 from gnn_baselines.gnn_architectures import GIN, GINE, GC
 
 def main():
-    num_reps = 10
+    num_reps = 3
 
     ### Smaller datasets.
     dataset = [["IMDB-BINARY", False], ["IMDB-MULTI", False], ["NCI1", True], ["PROTEINS", True],
@@ -15,7 +15,7 @@ def main():
         dp.get_dataset(d)
 
         # GIN, dataset d, layers in [1:6], hidden dimension in {32,64,128}.
-        acc, s_1, s_2 = gnn_evaluation(GIN, d, [1, 2, 3, 4, 5], [32, 64, 128], max_num_epochs=200, batch_size=64,
+        acc, s_1, s_2 = gnn_evaluation(GIN, d, [3], [64], max_num_epochs=200, batch_size=64,
                                        start_lr=0.01, num_repetitions=num_reps, all_std=True)
 
         print(d + " " + "GIN " + str(acc) + " " + str(s_1) + " " + str(s_2))
