ade215d1599aa055ad70b594894649e1076cb44f
Christopher Morris <christopher.morris@udo.edu>
2023-03-25 17:19:23 +0100

    - Added switch for making GNN parameter untrained.

--- GNN_untrained/main_gnn.py
+++ GNN_untrained/main_gnn.py
@@ -14,6 +14,20 @@ def main():
         # Download dataset.
         dp.get_dataset(d)
 
+        acc, s_1, s_2 = gnn_evaluation(GC, d, [3], [64], max_num_epochs=200, batch_size=64,
+                                       start_lr=0.01, num_repetitions=num_reps, all_std=True, untrain=False)
+
+        print(d + " " + "GraphConv " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "GraphConv " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+        acc, s_1, s_2 = gnn_evaluation(GC, d, [3], [64], max_num_epochs=200, batch_size=64,
+                                       start_lr=0.01, num_repetitions=num_reps, all_std=True, untrain=True)
+
+        print(d + " " + "GraphConv " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "GraphConv " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+
+
         # GIN, dataset d, layers in [1:6], hidden dimension in {32,64,128}.
         acc, s_1, s_2 = gnn_evaluation(GIN, d, [3], [64], max_num_epochs=200, batch_size=64,
                                        start_lr=0.01, num_repetitions=num_reps, all_std=True, untrain=False)
