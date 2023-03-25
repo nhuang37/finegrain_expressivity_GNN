6e97bf684b254834f7fbddd9a8bd3293651f3860
Christopher Morris <christopher.morris@udo.edu>
2023-03-25 16:50:15 +0100

    - Addec GCN architectures

--- GNN_untrained/gnn_baselines/gnn_architectures.py
+++ GNN_untrained/gnn_baselines/gnn_architectures.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 from torch.nn import Linear, Sequential, ReLU, BatchNorm1d as BN
-from torch_geometric.nn import GINConv, GraphConv, global_add_pool
+from torch_geometric.nn import GINConv, GraphConv, GCNConv, GCNConv, global_add_pool
 from torch_geometric.nn import MessagePassing
 from torch_geometric.nn.inits import reset
 
@@ -124,6 +124,40 @@ class GC(torch.nn.Module):
         self.convs = torch.nn.ModuleList()
         for i in range(num_layers - 1):
             self.convs.append(GraphConv(dataset.num_features, hidden))
+        self.lin1 = Linear(hidden, hidden)
+        self.lin2 = Linear(hidden, dataset.num_classes)
+
+    def reset_parameters(self):
+        self.conv1.reset_parameters()
+        for conv in self.convs:
+            conv.reset_parameters()
+        self.lin1.reset_parameters()
+        self.lin2.reset_parameters()
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        x = self.conv1(x, edge_index)
+        for conv in self.convs:
+            x = conv(x, edge_index)
+
+        x = global_add_pool(x, batch)
+
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin2(x)
+        return F.log_softmax(x, dim=-1)
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+
+class GCN(torch.nn.Module):
+    def __init__(self, dataset, num_layers, hidden):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(dataset.num_features, hidden)
+        self.convs = torch.nn.ModuleList()
+        for i in range(num_layers - 1):
+            self.convs.append(GCNConv(dataset.num_features, hidden))
         self.lin1 = Linear(hidden, hidden)
         self.lin2 = Linear(hidden, dataset.num_classes)
 
