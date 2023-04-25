d22940ecfee27130cc4aedffcb301a75a2013a33
Ningyuan Huang, Teresa <35894842+nhuang37@users.noreply.github.com>
2023-04-25 16:08:00 -0400

    added GIN_meanpool, GC_meanpool to reflect the graph size normalization

--- GNN_untrained/gnn_baselines/gnn_architectures.py
+++ GNN_untrained/gnn_baselines/gnn_architectures.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn.functional as F
 from torch.nn import Linear, Sequential, ReLU, BatchNorm1d as BN
-from torch_geometric.nn import GINConv, GraphConv, GCNConv, global_add_pool
+from torch_geometric.nn import GINConv, GraphConv, GCNConv, global_add_pool, global_mean_pool
 from torch_geometric.nn import MessagePassing
 from torch_geometric.nn.inits import reset
 
+from torch_geometric.utils import degree #for graph size normalization in the meanpool achitectures
 
 # Taken from https://github.com/rusty1s/pytorch_geometric/blob/master/benchmark/kernel/gin.py.
 class GIN(torch.nn.Module):
@@ -66,6 +67,69 @@ class GIN(torch.nn.Module):
     def __repr__(self):
         return self.__class__.__name__
 
+
+class GIN_meanpool(torch.nn.Module):
+    def __init__(self, dataset, num_layers, hidden, untrain):
+        super(GIN_meanpool, self).__init__()
+        self.conv1 = GINConv(Sequential(
+            Linear(dataset.num_features, hidden),
+            ReLU(),
+            Linear(hidden, hidden),
+            ReLU(),
+            BN(hidden),
+        ),
+            train_eps=True)
+
+        self.untrain = untrain
+
+        self.convs = torch.nn.ModuleList()
+        for i in range(num_layers - 1):
+            self.convs.append(
+                GINConv(Sequential(
+                    Linear(hidden, hidden),
+                    ReLU(),
+                    Linear(hidden, hidden),
+                    ReLU(),
+                    BN(hidden),
+                ),
+                    train_eps=True))
+        self.lin1 = Linear(hidden, hidden)
+        self.lin2 = Linear(hidden, dataset.num_classes)
+
+    def reset_parameters(self):
+        self.conv1.reset_parameters()
+        for conv in self.convs:
+            conv.reset_parameters()
+        self.lin1.reset_parameters()
+        self.lin2.reset_parameters()
+
+        if self.untrain:
+            for param in self.conv1.parameters():
+                param.requires_grad = False
+
+            for conv in self.convs:
+                for param in conv.parameters():
+                    param.requires_grad = False
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        x = self.conv1(x, edge_index)
+        inv_graph_size = degree(batch, dtype=x.dtype).pow(-1)
+        x = x * inv_graph_size.index_select(0, batch).view(-1, 1)
+
+        for conv in self.convs:
+            x = conv(x, edge_index)
+            x = x * inv_graph_size.index_select(0, batch).view(-1, 1)
+
+        x = global_mean_pool(x, batch) #use mean pooling
+
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin2(x)
+        return F.log_softmax(x, dim=-1)
+
+    def __repr__(self):
+        return self.__class__.__name__
 
 class GINEConv(MessagePassing):
     def __init__(self, edge_dim, dim_init, dim):
@@ -172,6 +236,53 @@ class GC(torch.nn.Module):
             x = conv(x, edge_index)
 
         x = global_add_pool(x, batch)
+
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin2(x)
+        return F.log_softmax(x, dim=-1)
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+class GC_meanpool(torch.nn.Module):
+    def __init__(self, dataset, num_layers, hidden, untrain):
+        super(GC_meanpool, self).__init__()
+        self.conv1 = GraphConv(dataset.num_features, hidden)
+        self.convs = torch.nn.ModuleList()
+        for i in range(num_layers - 1):
+            self.convs.append(GraphConv(hidden, hidden))
+        self.lin1 = Linear(hidden, hidden)
+        self.lin2 = Linear(hidden, dataset.num_classes)
+
+        self.untrain = untrain
+
+    def reset_parameters(self):
+        self.conv1.reset_parameters()
+        for conv in self.convs:
+            conv.reset_parameters()
+        self.lin1.reset_parameters()
+        self.lin2.reset_parameters()
+
+        if self.untrain:
+            for param in self.conv1.parameters():
+                param.requires_grad = False
+
+            for conv in self.convs:
+                for param in conv.parameters():
+                    param.requires_grad = False
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        x = self.conv1(x, edge_index)
+        inv_graph_size = degree(batch, dtype=x.dtype).pow(-1)
+        x = x * inv_graph_size.index_select(0, batch).view(-1, 1)
+
+        for conv in self.convs:
+            x = conv(x, edge_index)
+            x = x * inv_graph_size.index_select(0, batch).view(-1, 1)
+
+        x = global_mean_pool(x, batch) #change it to mean pooling
 
         x = F.relu(self.lin1(x))
         x = F.dropout(x, p=0.5, training=self.training)
